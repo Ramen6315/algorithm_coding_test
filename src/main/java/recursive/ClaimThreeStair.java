4548ad9d1acfe3671758f1550364d350b6695143
ramen6315 <ramen6315@gmail.com>
2022-12-06 21:44:03 +0900

    feat : 계단오르기 Large 문제 풀이  - 재귀방식으로 풀었으나 메서드 호출 횟수가 뜨면 StackOverFlow 발생 -> 반복문으로 품 -> 모든 배열으 두는게 아니라 크기를 작게 해서 해쉬 값으로 위치를 계속 Update하면서 풀어야됨  - 해쉬 방식으로 메모리를 최소한으로 푸는 방법을 생각

---
+++ src/main/java/recursive/ClaimThreeStair.java
@@ -0,0 +1,29 @@
+package recursive;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ClaimThreeStair {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        BigDecimal[] memoization = new BigDecimal[3];
+
+        fibonachi(n, memoization);
+        System.out.println(memoization[n%3].remainder(BigDecimal.valueOf(1000)).intValue());
+    }
+
+    private static BigDecimal fibonachi(int n, BigDecimal[] memoization) {
+        for (int i = 0; i <= n; i++) {
+            if(i < 2) {
+                memoization[i % 3] = BigDecimal.valueOf(1);
+            } else if(i == 2) {
+                memoization[i % 3] = BigDecimal.valueOf(2);
+            } else {
+                memoization[i % 3] = memoization[i % 3].add(memoization[(i - 1) % 3]).add(memoization[(i - 2) % 3]);
+            }
+
+        }
+        return memoization[n % 3].remainder(BigDecimal.valueOf(1000));
+    }
+}
