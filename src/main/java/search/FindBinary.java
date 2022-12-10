24a85eba202c2b00cc060b2d8b0d67d70558816e
ramen6315 <ramen6315@gmail.com>
2022-12-10 18:13:07 +0900

    feat : 규칙에 맞는 이진수  - 점화식을 이용하여 푸는 문제

---
+++ src/main/java/search/FindBinary.java
@@ -0,0 +1,29 @@
+package search;
+
+import java.util.Scanner;
+
+public class FindBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] numbers = new int[number];
+        if(number == 1) {
+            numbers[0] = 2;
+        }
+        if(number >= 2){
+            numbers[0] = 2;
+            numbers[1] = 3;
+        }
+        System.out.println(findBinary(number - 1, numbers));
+    }
+
+    private static int findBinary(int number, int[] numbers) {
+        if(number < 2) {
+            return numbers[number];
+        }
+        for (int i = 2; i <= number; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
+        }
+        return numbers[number];
+    }
+}
