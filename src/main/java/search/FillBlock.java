9c2664a29fc6131f0ee722baacc764e90f303232
ramen6315 <ramen6315@gmail.com>
2022-12-10 18:11:42 +0900

    feat : 블록 채우기  - 점화식을 이용하여 푸는 문제

---
+++ src/main/java/search/FillBlock.java
@@ -0,0 +1,28 @@
+package search;
+
+import java.util.Scanner;
+
+public class FillBlock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int columns = scanner.nextInt();
+        int[] cases = new int[columns];
+        if(columns == 1) {
+            System.out.println(1);
+        } else if(columns == 2) {
+            System.out.println(2);
+        } else {
+            cases[0] = 1;
+            cases[1] = 2;
+            System.out.println(findFillBlockCases(columns, cases));
+        }
+
+    }
+
+    private static int findFillBlockCases(int columns, int[] cases) {
+        for (int i = 2; i < columns; i++) {
+            cases[i] = cases[i - 1] + cases[i - 2];
+        }
+        return cases[columns - 1];
+    }
+}
