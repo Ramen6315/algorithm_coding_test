4f10af3efb00420f2c3405228a62fb51e4ad1269
ramen6315 <ramen6315@gmail.com>
2022-12-15 21:45:29 +0900

    feat : 삼각화단 만들기 문제  - a,b,c값을 받아서 비교 및 재귀 시작 각 값을 1씩 증가 시키면서 3개의 재귀를 돌리면됨 -> 배열의 메모리가 많아 지는거 같아 최대한 줄여서 푸는 방법 고민

---
+++ src/main/java/search/TriangleFlowerBed.java
@@ -0,0 +1,14 @@
+package search;
+
+import java.util.Scanner;
+
+
+// 메모리를 많이 잡아먹지 않는 방법을 생각해서 풀어볼것
+public class TriangleFlowerBed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int cnt = 0;
+    }
+
+}
